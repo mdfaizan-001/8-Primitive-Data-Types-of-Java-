bc542b336f3b6a373ffa8cd922da45903b133c51
Md Faizan <mfrnmdfaizan@gmail.com>
2026-07-07 12:35:12 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte age = 20;
+        short marks = 500;
+        int salary = 50000;
+        long population = 140000000L;
+        float cgpa = 8.5f;
+        double pi = 3.14159;
+        char grade = 'A' ;
+        boolean isPass = true;
+
+        System.out.println(age);
+        System.out.println(marks);
+        System.out.println(salary);
+        System.out.println(population);
+        System.out.println(cgpa);
+        System.out.println(pi);
+        System.out.println(grade);
+        System.out.println(isPass);
+    }
+}
